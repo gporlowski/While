25e06944d5b49c010d00087aa227fbc74dab19c9
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-23 00:24:57 +0100

    While 3

--- While3.java
+++ While3.java
@@ -1,66 +1,32 @@
 import java.util.Scanner;
 
 public class While3 {
+    public static final int[] DENOMINATIONS = new int[] {200, 100, 50, 20, 10, 5, 2, 1};
+    public static final int AMOUNT_OF_BANKNOTES = 5;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj kwotę (liczba całkowitą): ");
+        System.out.print("Podaj kwotę pieniędzy (liczba całkowitą): ");
         int value = scan.nextInt();
-        System.out.println("Oto kombinacja nominałów, które zajmie najmniej miejsca w portfelu:");
-        twoHundred(value);
-        hundred(value);
-        fifty(value);
-        twenty(value);
-        ten(value);
-        five(value);
-        two(value);
-        one(value);
+        System.out.println("\nOto kombinacja nominałów, które zajmie najmniej miejsca w portfelu:");
+        simplestDenominations(value);
     }
-    public static void twoHundred (int value) {
-        value = value / 200;
-        if (value > 0) {
-            System.out.println(value + " x banknot 200 złotowy.");
+    public static void simplestDenominations (int value) {
+        int i = 0;
+        int howMuchMoney;
+        while (value > 0) {
+            howMuchMoney = value / DENOMINATIONS[i];
+            if (howMuchMoney > 0) {
+                printAmountMoney(howMuchMoney, i);
+            }
+            value %= DENOMINATIONS[i];
+            i++;
         }
     }
-    public static void hundred (int value) {
-        value = (value % 200) / 100;
-        if (value > 0) {
-            System.out.println(value + " x banknot 100 złotowy.");
-        }
-    }
-    public static void fifty (int value) {
-        value = (value % 100) / 50;
-        if (value > 0) {
-            System.out.println(value + " x banknot 50 złotowy.");
-        }
-    }
-    public static void twenty (int value) {
-        value = (value % 50) / 20;
-        if (value > 0) {
-            System.out.println(value + " x banknot 20 złotowy.");
-        }
-    }
-    public static void ten (int value) {
-        value = ((value % 50) % 20) / 10;
-        if (value > 0) {
-            System.out.println(value + " x banknot 10 złotowy.");
-        }
-    }
-    public static void five (int value) {
-        value = (value % 10) / 5;
-        if (value > 0) {
-            System.out.println(value + " x moneta 5 złotowa");
-        }
-    }
-    public static void two (int value) {
-        value = ((value % 10) % 5) / 2;
-        if (value > 0) {
-            System.out.println(value + " x moneta 2 złotowa");
-        }
-    }
-    public static void one (int value) {
-        value = ((value % 10) % 5) % 2;
-        if (value > 0) {
-            System.out.println(value + " x moneta 1 złotowa");
+    public static void printAmountMoney (int howMuchMoney, int i) {
+        if (i < AMOUNT_OF_BANKNOTES) {
+            System.out.println(howMuchMoney + " x banknot " + DENOMINATIONS[i] + " złotowy.");
+        } else {
+            System.out.println(howMuchMoney + " x moneta " + DENOMINATIONS[i] + " złotowa.");
         }
     }
 }
